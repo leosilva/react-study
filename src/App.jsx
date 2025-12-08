882a9640ffdedcffbda5bfb685951ba50699e2b8
Leo Moreira Silva <leo.moreira@me.com>
2025-12-08 09:24:36 -0300

    initial import

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Alert } from './Alert';
+
+function App() {
+   return (
+      <div>
+         <Alert type="information" heading="Success">
+            Everything is really good!
+         </Alert>
+      </div>
+   );
+}
+
+export default App;
