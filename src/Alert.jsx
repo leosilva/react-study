3899bc23882c4a0219610278a478b69e318bffd4
Leo Moreira Silva <leo.moreira@me.com>
2025-12-12 17:06:48 -0300

    alteracoes no componente Alert

--- src/Alert.jsx
+++ src/Alert.jsx
@@ -1,4 +1,16 @@
-export function Alert({ type = 'information', heading, children }) {
+import { useState } from 'react';
+
+export function Alert({ type = 'information', heading, children, closable, onClose }) {
+   const [visible, setVisible] = useState(true);
+   if (!visible) {
+      return null;
+   }
+   function handleCloseClick() {
+      setVisible(false);
+      if (onClose) {
+         onClose();
+      }
+   }
    return (
       <div>
          <div>
@@ -7,6 +19,13 @@ export function Alert({ type = 'information', heading, children }) {
             </span>
             <span>{heading}</span>
          </div>
+         {closable && (
+            <button aria-label="Close" onClick={handleCloseClick}>
+               <span role="img" aria-label="Close">
+                  ❌
+               </span>
+            </button>
+         )}
          <div>{children}</div>
       </div>
    );
--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Alert } from './Alert';
 function App() {
    return (
       <div>
-         <Alert type="information" heading="Success">
+         <Alert type="information" heading="Success" closable onClose={() => console.log('closed')}>
             Everything is really good!
          </Alert>
       </div>
