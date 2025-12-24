bd139682663ff7053517e5e1720a6bebfb2244e6
Leo Moreira Silva <leo.moreira@me.com>
2025-12-23 22:01:53 -0300

    chapter 2, alert component with tsx

---
+++ alert/src/App.tsx
@@ -0,0 +1,14 @@
+import { Alert } from './Alert';
+import './App.css';
+
+
+function App() {
+  return (
+    <Alert heading='Success'>
+      Everything is really good!
+    </Alert>
+  )
+    
+}
+
+export default App;
